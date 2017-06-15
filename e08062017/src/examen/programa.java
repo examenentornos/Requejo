0cc1ac1d596f4694ab4b162ceeaecdd3121fc38a
Requejo <pablogr_1996@hotmail.es>
2017-06-15 08:47:41 +0200

    Commit primero

---
+++ e08062017/src/examen/programa.java
@@ -0,0 +1,57 @@
+package examen;
+
+import java.util.Scanner;
+
+public class programa {
+
+	private static final int com1 = 1000;
+	private static final int com2 = 500;
+	private static final int com3 = 300;
+	private static final int com4 = 100;
+
+	public static int Usuario(String codigo, String nombre, int antiguedad, boolean plus) {
+
+		if ((codigo.length() > 3) || (codigo.length() < 3) || (codigo == "000")) {
+			throw new RuntimeException();
+		}
+
+		if ((nombre.length() > 20) || (nombre.length() == 0)) {
+			throw new RuntimeException();
+		}
+
+		if ((antiguedad > 2) || (antiguedad < 2)) {
+			throw new RuntimeException();
+		}
+
+		int complemento = 0;
+
+		if (antiguedad >= 5 && plus == true) {
+			complemento = com1;
+		}
+		if (antiguedad >= 5 && plus == false) {
+			complemento = com2;
+		}
+		if (antiguedad <= 5 && plus == true) {
+			complemento = com3;
+		}
+		if (antiguedad <= 5 && plus == false) {
+			complemento = com4;
+		}
+		
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner escaner = new Scanner(System.in);
+	    escaner.useDelimiter(";");
+	    String codigo = escaner.next();
+	    String nombre = escaner.next();
+	    String antiguedad = escaner.next();
+	    String plus = escaner.next();
+	    
+	    if (Usuario(codigo, nombre, 0, false) == 0) {
+	    	throw new RuntimeException();
+	      }
+	    }
+	}
